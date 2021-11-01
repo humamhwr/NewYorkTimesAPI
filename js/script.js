c27ec4fa4e5cd378fde1243d8c8477128c521466
sharon-ku <71299609+sharon-ku@users.noreply.github.com>
2021-11-01 19:33:31 -0400

    Deleted code.js; removed search term; removed end year; removed "Clear Results" button code

--- js/script.js
+++ js/script.js
@@ -14,26 +14,29 @@ function executeJSON() {
   //   "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=20100101&end_date=20211031&api-key=SAiMm3TGavwAZGVZQQXSSYnpj8jUUIdF";
 
   // Grab form values
-  let startYear = document.getElementById("startYear").value;
-  let endYear = document.getElementById("endYear").value;
+  let year = document.getElementById("year").value;
+  // let endYear = document.getElementById("endYear").value;
   let pageNumber = getRandomInt(10);
 
   let numRecords = document.getElementById("numRecords").value;
 
-  console.log(numRecords, startYear, endYear);
+  console.log(numRecords, year);
 
-  const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=${startYear}0101&end_date=${endYear}1231&page=${pageNumber}&api-key=SAiMm3TGavwAZGVZQQXSSYnpj8jUUIdF`;
+  // const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=${startYear}0101&end_date=${endYear}1231&page=${pageNumber}&api-key=SAiMm3TGavwAZGVZQQXSSYnpj8jUUIdF`;
+  const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=${year}0101&page=${pageNumber}&api-key=SAiMm3TGavwAZGVZQQXSSYnpj8jUUIdF`;
 
   $.getJSON(url, function (data) {
     //success
     console.log(data);
-    // for (let i = 0; i < data.response.docs.length; i++) {
-    for (let i = 0; i < numRecords; i++) {
-      console.log(data.response.docs[i].headline.main);
 
+    for (let i = 0; i < numRecords; i++) {
       // Display retrieved articles
+      // Headline
       document.getElementById("retrievedArticles").innerText +=
         data.response.docs[i].headline.main;
+      // Section name
+      document.getElementById("retrievedArticles").innerText +=
+        data.response.docs[i].section_name;
     }
   })
     //fail
