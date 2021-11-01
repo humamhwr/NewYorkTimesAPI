6095e0d6d07be8a696e02942870285132c8ac618
sharon-ku <71299609+sharon-ku@users.noreply.github.com>
2021-11-01 11:49:34 -0400

    Got input start year and end year working; changed number of records to retrieve, removed "Clear" button and "search term" sections

--- js/script.js
+++ js/script.js
@@ -1,37 +1,38 @@
 /*******************
 Fetch json data
-
-
 ********************/
+function executeJSON() {
+  // const url =
+  // "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=SAiMm3TGavwAZGVZQQXSSYnpj8jUUIdF";
 
-function execute() {
-  const url =
-    "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=SAiMm3TGavwAZGVZQQXSSYnpj8jUUIdF";
-  const options = {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-    },
-  };
-  fetch(url, options)
-    .then((response) => {
-      if (response.ok) {
-        return response.text();
-      }
-      return response.text().then((err) => {
-        return Promise.reject({
-          status: response.status,
-          statusText: response.statusText,
-          errorMessage: err,
-        });
-      });
-    })
-    .then((data) => {
-      console.log(data);
-      // console.log(data.status);
-      // console.log(data.response.docs[0].abstract);
-    })
-    .catch((err) => {
-      console.error(err);
+  // const url =
+  //   "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=20100101&end_date=20211031&api-key=SAiMm3TGavwAZGVZQQXSSYnpj8jUUIdF";
+
+  let startYear = document.getElementById("startYear").value;
+  let endYear = document.getElementById("endYear").value;
+  let pageNumber = getRandomInt(10);
+
+  let numRecords = document.getElementById("numRecords").value;
+
+  console.log(numRecords, startYear, endYear);
+
+  const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=${startYear}0101&end_date=${endYear}1231&page=${pageNumber}&api-key=SAiMm3TGavwAZGVZQQXSSYnpj8jUUIdF`;
+
+  $.getJSON(url, function (data) {
+    //success
+    console.log(data);
+    // for (let i = 0; i < data.response.docs.length; i++) {
+    for (let i = 0; i < numRecords; i++) {
+      console.log(data.response.docs[i].headline.main);
+    }
+  })
+    //fail
+    .fail(function () {
+      console.log("error");
     });
+}
+
+// Pull random integer given the maximum number
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
