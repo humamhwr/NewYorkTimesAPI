41b3f235968846771cc8d1d2159c9f72db1de73c
humamhawara <hummam.houara@mail.concordia.ca>
2021-11-09 00:47:53 -0500

    push

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ Fetch json data and display result in "Articles Found"
 function executeJSON() {
   // Empty Top Articles text
   document.getElementById("retrievedArticles").innerText = "";
-  document.getElementById("retrievedArticlesSec").innerText = "";
+  // document.getElementById("retrievedArticlesSec").innerText = "";
 
   // Grab form values
   let year = document.getElementById("year").value;
@@ -16,23 +16,32 @@ function executeJSON() {
   const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=${year}0101&page=${pageNumber}&api-key=SAiMm3TGavwAZGVZQQXSSYnpj8jUUIdF`;
 
   $.getJSON(url, function(data) {
-      //success
-      console.log(data);
+    //success
+    console.log(data);
 
-      // Display retrieved articles
-      for (let i = 0; i < numRecords; i++) {
-        // Display headline
-        document.getElementById("retrievedArticles").innerText +=
-          data.response.docs[i].headline.main;
-        // Display section name
-        document.getElementById("retrievedArticlesSec").innerText +=
-          data.response.docs[i].section_name;
-      }
-    })
-    //fail
-    .fail(function() {
-      console.log("error");
-    });
+    // Display retrieved articles
+    for (let i = 0; i < numRecords; i++) {
+      // Display headline
+
+      let retrievedArticle = document.createElement("P");
+      let t = document.createTextNode(data.response.docs[i].headline.main);
+      retrievedArticle.appendChild(t);
+      document.getElementById("retrievedArticles").appendChild(retrievedArticle);
+      retrievedArticle.classList.add("StyleArticle");
+
+  let retrievedArticleSec = document.createElement("P");
+  let t1 = document.createTextNode(data.response.docs[i].section_name);
+  retrievedArticleSec.appendChild(t1);
+  document.getElementById("retrievedArticles").appendChild(retrievedArticleSec);
+  retrievedArticleSec.classList.add("StyleArticleSec");
+}
+})
+
+
+//fail
+.fail(function() {
+  console.log("error");
+});
 }
 
 // Pull random integer given the maximum number
