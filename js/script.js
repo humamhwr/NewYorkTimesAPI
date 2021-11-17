9eb3e38a9266b4f66d61a2cf1de92e6cae73c684
humamhawara <hummam.houara@mail.concordia.ca>
2021-11-16 23:26:48 -0500

    last update

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ Fetch json data and display result in "Articles Found"
 function executeJSON() {
   // Empty Top Articles text
   document.getElementById("retrievedArticles").innerText = "";
-  // document.getElementById("retrievedArticlesSec").innerText = "";
 
   // Grab form values
   let year = document.getElementById("year").value;
@@ -13,7 +12,7 @@ function executeJSON() {
   let pageNumber = getRandomInt(10);
 
   // URL to API JSON file
-  const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=${year}0101&page=${pageNumber}&api-key=SAiMm3TGavwAZGVZQQXSSYnpj8jUUIdF`;
+  const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?${year}0101&end_date=${year}1231&page=${pageNumber}&api-key=SAiMm3TGavwAZGVZQQXSSYnpj8jUUIdF`;
 
   $.getJSON(url, function(data) {
     //success
@@ -29,13 +28,13 @@ function executeJSON() {
       document.getElementById("retrievedArticles").appendChild(retrievedArticle);
       retrievedArticle.classList.add("StyleArticle");
 
-  let retrievedArticleSec = document.createElement("P");
-  let t1 = document.createTextNode(data.response.docs[i].section_name);
-  retrievedArticleSec.appendChild(t1);
-  document.getElementById("retrievedArticles").appendChild(retrievedArticleSec);
-  retrievedArticleSec.classList.add("StyleArticleSec");
-}
-})
+      let retrievedArticleSec = document.createElement("P");
+      let t1 = document.createTextNode(data.response.docs[i].section_name);
+      retrievedArticleSec.appendChild(t1);
+      document.getElementById("retrievedArticles").appendChild(retrievedArticleSec);
+      retrievedArticleSec.classList.add("StyleArticleSec");
+    }
+  })
 
 
 //fail
